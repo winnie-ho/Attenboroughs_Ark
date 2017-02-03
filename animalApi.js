4239fdd1c4285252a2c348173f426700fae25eb0
Lewis MacNee <lewis.macnee@hotmail.com>
2017-02-03 11:24:44 +0000

    Added african elephant to animals API

--- animalApi.js
+++ animalApi.js
@@ -9,8 +9,19 @@ db.animals.insert([
     2: "this animal eats almost only bamboo!",
     3: "this animal appears to be a species of bear!"
   },
-  answerText: "I seem to remember coming across this vast creature upon my previous travels in the mountains of central China, It seems to be a Giant Panda! Let's take them back to our boat to take care",
+  answerText: "I seem to remember coming across this vast creature upon my previous travels in the mountains of central China, It seems to be a Giant Panda! Let's take him back to our boat for care",
   image: "http://multimedia.scmp.com/widgets/jia-jia/images/JiaJia.png"
+},
+{
+  name: "African Elephant",
+  country: "West Africa",
+  questions: {
+    1: "this animal seems to be very Large!",
+    2: "this animal has a very tough skin!",
+    3: "this animal has a long nose and is very wrinkled, even more so than I am!"
+  },
+  answerText: "I remember encountering this animal before, and I'm sure that she remembers me too! an African Elephant has an exceptional memory after all!",
+  image: "http://www.pngall.com/wp-content/uploads/2016/06/Elephant-PNG.png"
 }
 ]);
 
