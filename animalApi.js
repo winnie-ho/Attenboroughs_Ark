6c49af925bc356e26f23115e583fd4ba4005ac04
Lewis MacNee <lewis.macnee@hotmail.com>
2017-02-03 12:48:31 +0000

    Almost completed finishing text for animals currently added

--- animalApi.js
+++ animalApi.js
@@ -9,19 +9,21 @@ db.animals.insert([
     2: "this animal eats almost only bamboo!",
     3: "this animal appears to be a species of bear!"
   },
-  answerText: "I seem to remember coming across this vast creature upon my previous travels in the mountains of central China, It seems to be a Giant Panda! Let's take him back to our boat for care",
-  image: "http://multimedia.scmp.com/widgets/jia-jia/images/JiaJia.png"
+  answerText: "I seem to remember coming across this large bear upon my previous travels in the mountains of central China, It seems to be a Giant Panda! Let's take a picture of this Panda and leave him in peace",
+  image: "http://multimedia.scmp.com/widgets/jia-jia/images/JiaJia.png",
+  finishingText: "We've found a Giant Panda! This large bear is only found in the remote, mountainous regions of the Sichuan, Shaanxi and Gansu provinces. These animals spend around 12 hours a day just eating! I think I would be ill if I were to eat for that long... As well as the bamboo, these Panda bears also eat bugs, fish and rodents, though bamboo is still the favourite. Let us take a picture of this Panda so we can leave him in peace to eat."
 },
 {
   name: "African Elephant",
   country: "West Africa",
   questions: {
-    1: "this animal seems to be very Large!",
-    2: "this animal has a very tough skin!",
-    3: "this animal has a long nose and is very wrinkled, even more so than I am!"
+    "one": "this animal seems to be very Large!",
+    "two": "this animal has a very tough skin!",
+    "three": "this animal has a long nose and is very wrinkled, even more so than I am!"
   },
-  answerText: "I remember encountering this animal before, and I'm sure that she remembers me too! an African Elephant has an exceptional memory after all!",
-  image: "http://www.pngall.com/wp-content/uploads/2016/06/Elephant-PNG.png"
+  answerText: "I remember encountering this animal before, and I'm sure that she remembers me too! an African Elephant has an exceptional memory after all! Lets take a picture and leave her to catch up with her heard.",
+  image: "http://www.pngall.com/wp-content/uploads/2016/06/Elephant-PNG.png",
+  finishingText: "We've found an African Elephant! Did you know that elephants are some of the largest land animals on the entire planet? The African Elephant uses its long trunk to pick up water, then it sprays the water into its mouth for a drink, or if the Elephant isn't thirsty, it can use it's trunk as a large hose, and will cool itself off with the spray of water."
 }
 ]);
 
