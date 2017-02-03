251bdee7bb156d205e0e8fa76231b7b0d95413ce
Lewis MacNee <lewis.macnee@hotmail.com>
2017-02-03 11:06:32 +0000

    Added a Giant Panda to animal API

---
+++ animalApi.js
@@ -0,0 +1,16 @@
+use attenboroughs_ark;
+
+db.animals.insert([
+{
+  name: "Giant Panda",
+  country: "China",
+  questions: {
+    1: "this animal seems to be Black and White.",
+    2: "this animal eats almost only bamboo!",
+    3: "this animal appears to be a species of bear!"
+  },
+  answerText: "I seem to remember coming across this vast creature upon my previous travels in the mountains of central China, It seems to be a Giant Panda! Let's take them back to our boat to take care",
+  image: "http://multimedia.scmp.com/widgets/jia-jia/images/JiaJia.png"
+}
+]);
+
