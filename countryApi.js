19e5336fbff7ee19ce886d941d848101f6e03a98
Lewis MacNee <lewis.macnee@hotmail.com>
2017-02-03 13:48:52 +0000

    Added western africa to countries api

---
+++ countryApi.js
@@ -0,0 +1,10 @@
+use attenboroughs_ark;
+
+db.countries.insert([
+{
+  name: "West Africa",
+  coords: [4.1272853, 17.094727],
+  arrivalText: "This, is Africa. In particular, we are in the central African Republic, though the endangered species we are looking for on this adventure can be found all down the western coast of this vast continent. As we travel across this beautiful  landscape, there are many wonderous sights. From the Hippopotamous, with jaws around 15-18 times stronger than yours, to the small Fenec fox, who hunts for rodents at night. Let us journey on, and explore the reaches of Western Africa in search of our endangered species today."
+}
+
+  ]);
