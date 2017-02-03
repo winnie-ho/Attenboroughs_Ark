d440f4b9e2564cf403cafd791a675c1efecd083e
Lewis MacNee <lewis.macnee@hotmail.com>
2017-02-03 14:06:07 +0000

    Completed China in countries API

--- countryApi.js
+++ countryApi.js
@@ -4,7 +4,12 @@ db.countries.insert([
 {
   name: "West Africa",
   coords: [4.1272853, 17.094727],
-  arrivalText: "This, is Africa. In particular, we are in the central African Republic, though the endangered species we are looking for on this adventure can be found all down the western coast of this vast continent. As we travel across this beautiful  landscape, there are many wonderous sights. From the Hippopotamous, with jaws around 15-18 times stronger than yours, to the small Fenec fox, who hunts for rodents at night. Let us journey on, and explore the reaches of Western Africa in search of our endangered species today."
+  arrivalText: "This, is Africa. In particular, we are in the central African Republic, though the endangered species we are looking for on this adventure can be found all down the western coast of this vast continent. As we travel across this beautiful  landscape, there are many wonderous sights. From the Hippopotamous, with jaws around 15-18 times stronger than yours, to the small Fenec fox, who hunts for rodents at night. Let us journey on, and explore the reaches of Western Africa in search of our endangered species today. "
+},
+{
+  name: "China",
+  coords: [31.51299585745468, 103.5845947265625],
+  arrivalText: "This, is China. In particular, we have travelled into the Mountains of the Sichaun province. The species we are looking for can be difficult to find, as there are very few of them left in the wild today. As we head out upon our travels across this part of the world, keep a lookout for the golden snub-nose monkey, as they run along the branches above your heads. Or if you're lucky as we walk along, you might be able to catch a brief glimpse of a Sichan Takin, or Gnu Goat. But for now, let us continue out journey across the Mountains, and explore the forests of Sichan province in search of our endangered species today. "
 }
 
   ]);
