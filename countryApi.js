775ec919e51a4e9d0035b3fb3505eb0d6c6d3323
Lewis MacNee <lewis.macnee@hotmail.com>
2017-02-04 12:31:51 +0000

    .

--- countryApi.js
+++ countryApi.js
@@ -9,7 +9,7 @@ db.countries.insert([
 {
   name: "China",
   coords: [31.51299585745468, 103.5845947265625],
-  arrivalText: "This, is China. In particular, we have travelled into the Mountains of the Sichuan province. The species we are looking for can be difficult to find, as there are very few of them left in the wild today. As we head out upon our travels across this part of the world, keep a lookout for the golden snub-nose monkey, as they run along the branches above your heads. Or if you're lucky as we walk along, you might be able to catch a brief glimpse of a Sichuan Takin, or Gnu Goat. But for now, let us continue out journey across the Mountains, and explore the forests of Sichuan province in search of our endangered species today. "
+  arrivalText: "This, is China. In particular, we have travelled into the Mountains of the Sichaun province. The species we are looking for can be difficult to find, as there are very few of them left in the wild today. As we head out upon our travels across this part of the world, keep a lookout for the golden snub-nose monkey, as they run along the branches above your heads. Or if you're lucky as we walk along, you might be able to catch a brief glimpse of a Sichan Takin, or Gnu Goat. But for now, let us continue out journey across the Mountains, and explore the forests of Sichan province in search of our endangered species today. "
 }
 
   ]);
