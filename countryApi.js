711914c98fd0e0e1a2aa69f0fbd7b25f259bf2a0
Lewis MacNee <lewis.macnee@hotmail.com>
2017-02-06 13:50:09 +0000

    Added Mexico

--- countryApi.js
+++ countryApi.js
@@ -18,6 +18,11 @@ db.countries.insert([
   coords: [28.394857,84.124008],
   arrivalText: "This, is Nepal, home to some of the highest mountain ranges on the planet. Any animals here have to be specially adapted for the extreme cold, and thin air caused by this height above sea level. As we travel across the mountains, keep on the lookout for some of the native species who make their home here, such as the Musk Deer, used to make some perfumes, or the Red Panda, hanging onto bamboo or hollow trees. For now though, let us travel across these massive mountains in search of our endangered species.",
   countryFlag: "https://upload.wikimedia.org/wikipedia/commons/d/da/Flag_of_Nepal.png"
+},
+{
+  name: "Mexico",
+  coords: [23.634501,-102.55278399999997],
+  arrivalText: "This, is Mexico, home to some of the most weird and wonderful animals of the Americas. Across Mexico there are varied climates, from intense heat on the lowlands in the north-west, to the much milder regions in the mountains of the central regions. This enables a large variety of wildlife to survive, though the expanse of cities in Mexico mean a lot of the wildlife here is losing their homes. Let us go and search for our endangered species today, in the hopes of allowing it's home to become a protected area."
 }
 
 ]);
