b950e2931b71603127479d2619a8dbf56491b1f0
Lewis MacNee <lewis.macnee@hotmail.com>
2017-02-06 11:29:19 +0000

    Added flags to countries

--- countryApi.js
+++ countryApi.js
@@ -4,17 +4,20 @@ db.countries.insert([
 {
   name: "West Africa",
   coords: [4.1272853, 17.094727],
-  arrivalText: "This, is Africa. In particular, we are in the central African Republic, though the endangered species we are looking for on this adventure can be found all down the western coast of this vast continent. As we travel across this beautiful  landscape, there are many wonderous sights. From the Hippopotamous, with jaws around 15-18 times stronger than yours, to the small Fenec fox, who hunts for rodents at night. Let us journey on, and explore the reaches of Western Africa in search of our endangered species today. "
+  arrivalText: "This, is Africa. In particular, we are in the central African Republic, though the endangered species we are looking for on this adventure can be found all down the western coast of this vast continent. As we travel across this beautiful  landscape, there are many wonderous sights. From the Hippopotamous, with jaws around 15-18 times stronger than yours, to the small Fenec fox, who hunts for rodents at night. Let us journey on, and explore the reaches of Western Africa in search of our endangered species today. ",
+  countryFlag: "https://upload.wikimedia.org/wikipedia/commons/e/e6/Flag_of_the_Central_African_Republic.png"
 },
 {
   name: "China",
   coords: [31.51299585745468, 103.5845947265625],
-  arrivalText: "This, is China. In particular, we have travelled into the Mountains of the Sichaun province. The species we are looking for can be difficult to find, as there are very few of them left in the wild today. As we head out upon our travels across this part of the world, keep a lookout for the golden snub-nose monkey, as they run along the branches above your heads. Or if you're lucky as we walk along, you might be able to catch a brief glimpse of a Sichan Takin, or Gnu Goat. But for now, let us continue our journey across the Mountains, and explore the forests of Sichan province in search of our endangered species today. "
+  arrivalText: "This, is China. In particular, we have travelled into the Mountains of the Sichaun province. The species we are looking for can be difficult to find, as there are very few of them left in the wild today. As we head out upon our travels across this part of the world, keep a lookout for the golden snub-nose monkey, as they run along the branches above your heads. Or if you're lucky as we walk along, you might be able to catch a brief glimpse of a Sichan Takin, or Gnu Goat. But for now, let us continue our journey across the Mountains, and explore the forests of Sichan province in search of our endangered species today. ",
+  countryFlag: "https://upload.wikimedia.org/wikipedia/commons/2/2e/Flag_of_China.png"
 },
 {
   name: "Nepal",
   coords: [28.394857,84.124008],
-  arrivalText: "This, is Nepal, home to some of the highest mountain ranges on the planet. Any animals here have to be specially adapted for the extreme cold, and thin air caused by this height above sea level. As we travel across the mountains, keep on the lookout for some of the native species who make their home here, such as the Musk Deer, used to make some perfumes, or the Red Panda, hanging onto bamboo or hollow trees. For now though, let us travel across these massive mountains in search of our endangered species."
+  arrivalText: "This, is Nepal, home to some of the highest mountain ranges on the planet. Any animals here have to be specially adapted for the extreme cold, and thin air caused by this height above sea level. As we travel across the mountains, keep on the lookout for some of the native species who make their home here, such as the Musk Deer, used to make some perfumes, or the Red Panda, hanging onto bamboo or hollow trees. For now though, let us travel across these massive mountains in search of our endangered species.",
+  countryFlag: "https://upload.wikimedia.org/wikipedia/commons/d/da/Flag_of_Nepal.png"
 }
 
 ]);
