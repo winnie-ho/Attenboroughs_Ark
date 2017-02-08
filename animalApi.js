498b2af7f38823b5a3cdcd58e625dde78801ac0b
Winnie Ho <winnie-ho@hotmail.co.uk>
2017-02-08 11:25:35 +0000

    spell checked the APIs

--- animalApi.js
+++ animalApi.js
@@ -5,9 +5,9 @@ db.animals.insert([
   name: "Giant Panda",
   country: "China",
   questions: {
-    one: "this animal seems to be Black and White.",
-    two: "this animal eats almost only bamboo!",
-    three: "this animal appears to be a species of bear!"
+    one: "This animal seems to be Black and White.",
+    two: "This animal eats almost only bamboo!",
+    three: "This animal appears to be a species of bear!"
   },
   answerText: "I seem to remember coming across this large bear upon my previous travels in the mountains of central China, It seems to be a Giant Panda! Let's take a picture of this Panda and leave him in peace. \n Let's choose another country to visit!",
   image: "http://multimedia.scmp.com/widgets/jia-jia/images/JiaJia.png",
@@ -18,13 +18,13 @@ db.animals.insert([
   name: "African Elephant",
   country: "West Africa",
   questions: {
-    one: "this animal seems to be very Large!",
-    two: "this animal has a very tough skin!",
-    three: "this animal has a long nose and is very wrinkled, even more so than I am!"
+    one: "This animal seems to be very Large!",
+    two: "This animal has a very tough skin!",
+    three: "This animal has a long nose and is very wrinkled, even more so than I am!"
   },
-  answerText: "I remember encountering this animal before, and I'm sure that she remembers me too! an African Elephant has an exceptional memory after all! Lets take a picture and leave her to catch up with her herd. \n Let's choose another country to visit!",
+  answerText: "I remember encountering this animal before, and I'm sure that she remembers me too! An African Elephant has an exceptional memory after all! Lets take a picture and leave her to catch up with her herd. \n Let's choose another country to visit!",
   image: "http://www.pngall.com/wp-content/uploads/2016/06/Elephant-PNG.png",
-finishingText: "We've found an African Elephant! Did you know that elephants are some of the largest land animals on the entire planet? \n The African Elephant uses its long trunk to pick up water, then it sprays the water into its mouth for a drink. \n If the Elephant isn't thirsty, it can also use it's trunk as a large hose, and will cool itself off with the spray of water. Let us take a picture of her and leave her to catch up with her heard. \n Let's choose another country to visit!",
+finishingText: "We've found an African Elephant! Did you know that elephants are some of the largest land animals on the entire planet? \n The African Elephant uses its long trunk to pick up water, then it sprays the water into its mouth for a drink. \n If the Elephant isn't thirsty, it can also use it's trunk as a large hose, and will cool itself off with the spray of water. Let us take a picture of her and leave her to catch up with her heard. \n Choose another country to visit!",
   buttonValues: ["Impala", "African Elephant", "Zebra", "Crested Porcupine", "Lion", "Tiger"]
   
 },
@@ -37,9 +37,9 @@ finishingText: "We've found an African Elephant! Did you know that elephants are
     two: "This animal has a thick white coat, with black or brown spots",
     three: "This animal makes its home high in the mountains, in the snowy landscapes"
   },
-  answerText: "I have seen this animal before! This reclusive animal is a Snow Leopard, it's thick fur helps protect it in the cold climates where it lives, this is a vary difficult animal to find, let us take a picture and quietly walk away so we can leave her to go and hunt. \n Let's choose another country to visit!",
+  answerText: "I have seen this animal before! This reclusive animal is a Snow Leopard, it's thick fur helps protect it in the cold climates where it lives. This is a vary difficult animal to find, let us take a picture and quietly walk away so we can leave her to go and hunt. \n Choose another country to visit!",
   image: "http://orig12.deviantart.net/9a07/f/2013/324/b/d/snow_leopard_png_by_laki10-d6v11th.png",
-  finishingText: "We've found ourselves a Snow Leopard! This big cat spends its entire life high up in the mountains, about 9'800 - 17'000 feet up. \n Up here the air is so thin that this big cat has to breathe deep to get enough air to survive. \n When hunting, a snow leopard can jump around 9 meters, thats almost the length of a bus! Let us take a picture and leave her in peace to go and hunt. \n Let's choose another country to visit!",
+  finishingText: "We've found ourselves a Snow Leopard! This big cat spends its entire life high up in the mountains, about 9'800 - 17'000 feet up. \n Up here the air is so thin that this big cat has to breathe deep to get enough air to survive. \n When hunting, a snow leopard can jump around 9 metres, that's almost the length of a bus! Let's take a picture and leave her in peace to go and hunt. \n Choose another country to visit!",
 
 buttonValues: ["Tahr", "Marmot", "Brown Bear", "Red Panda", "Snow Leopard", "Yak"]
 },
@@ -49,11 +49,11 @@ buttonValues: ["Tahr", "Marmot", "Brown Bear", "Red Panda", "Snow Leopard", "Yak
   questions: {
     one: "This animal appears to be a type of large Cat",
     two: "This big cat loves playing in the water!",
-    three: "this cat will sometimes be seen climbing a tree so as to leap down onto it's prey."
+    three: "This cat will sometimes be seen climbing a tree so as to leap down onto its prey."
   },
   answerText:"I remember seeing this animal on a trip into the forest once, it's a Jaguar! This big cat lives mostly alone, roaming around its territory. When it roars it sounds almost like a chesty cough. I've taken a picture, so we can leave him to sleep.  \n Let's choose another country to visit!",
   image: "http://www.pngall.com/wp-content/uploads/2016/04/Jaguar-PNG-Picture.png",
-  finishingText: "We've found ourselves a Jaguar! This big cat lives mostly alone, roaming through its territory. It has powerful jaws which it uses to bite through the skulls of most of the animals which are to become its prey. \n The Jaguar can live in a variety of habitats, from the forests and rainforests to the swamps and grasslands. \n Normally the jaguar has a yellow and tan coat, however they can have much darker coats as well, almost black in some instances. \n This cat looks tired, lets take a picture so he can get some sleep. \n Let's choose another country to visit!",
+  finishingText: "We've found ourselves a Jaguar! This big cat lives mostly alone, roaming through its territory. It has powerful jaws which it uses to bite through the skulls of most of the animals which are to become its prey. \n The Jaguar can live in a variety of habitats, from the forests and rainforests to the swamps and grasslands. \n Normally the jaguar has a yellow and tan coat, however they can have much darker coats as well, almost black in some instances. \n This cat looks tired, lets take a picture so he can get some sleep. \n Choose another country to visit!",
   buttonValues: ["Porcupine", "Capybara", "Llama", "Jaguar", "Tapir", "Coatimundi"]
 
 }
